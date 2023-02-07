fce0966faa427cc163a221e8b3589f1b8994f46c
Evgeny Semenov <efgen42@gmail.com>
2023-02-07 06:47:26 +0000

    edit run-puppet.sh

--- files/run-puppet.sh
+++ files/run-puppet.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#cd /etc/puppetlabs/code/environments/production && git pull
-cd /etc/puppetlabs/code/environments/puppet && git pull
-/opt/puppetlabs/bin/puppet apply manifests/
+cd /etc/puppetlabs/code/environments/production && git pull
+/usr/bin/puppet apply manifests/
